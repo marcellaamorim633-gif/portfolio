80dd3aaa4e104e70d8f1132f4a56908fe1b4a52f
marcellaamorim-18 <marcella.amorim@ssonic.com.br>
2026-07-13 16:27:36 -0300

    *

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def login():
+
+    if request.method == "POST":
+
+        email = request.form["email"]
+        senha = request.form["senha"]
+
+        # Login de teste
+        if email == "admin@astech.com" and senha == "123456":
+            return redirect(url_for("acesso"))
+
+        return "Usuário ou senha inválidos"
+
     return render_template("index.html")
+
+
+@app.route("/acesso")
+def acesso():
+    return render_template("acesso.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
